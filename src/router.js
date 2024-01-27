d3f136383480f9e768c9526df224117ad327f4e2
Guillermo Monasterios <gmonasterios@outlook.com>
2024-01-27 19:41:30 +0100

    rutas

--- src/router.js
+++ src/router.js
@@ -1,1 +1,17 @@
-import { createRouter, createWebHashHistory } from "vue-router"
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "./views/Home.vue";
+import Login from "./views/Login.vue";
+import Register from "./views/Register.vue";
+
+const routes = [ 
+    { path: "/", component: Home },
+    { path: "/login", component: Login },
+    { path: "/register", component: Register },
+];
+
+const router = createRouter({ 
+    routes,
+    history: createWebHistory(),
+})
+
+export default router;
